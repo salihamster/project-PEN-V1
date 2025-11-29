bfa461a99f10767422f64a7c968faf3ff8769ae9
Salih Mirza <salihmirza880@gmail.com>
2025-11-29 15:26:27 +0300

    Change API_BASE to use 127.0.0.1 instead of localhost

--- web/script.js
+++ web/script.js
@@ -1,4 +1,4 @@
-const API_BASE = "http://localhost:8000"; // Backend API endpointi buraya gelecek
+const API_BASE = "http://127.0.0.1:8000"; // Backend API endpointi buraya gelecek
 
 let isSending = false;
 let lastUserMessageAt = null;
