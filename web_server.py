e9e6ad628c893146f641a010392f31fb5490ffa2
Salih Mirza <salihmirza880@gmail.com>
2025-11-29 15:25:43 +0300

    Change server host from 0.0.0.0 to 127.0.0.1

--- web_server.py
+++ web_server.py
@@ -157,4 +157,4 @@ async def serve_static(full_path: str):
 if __name__ == "__main__":
     import uvicorn
 
-    uvicorn.run("web_server:app", host="0.0.0.0", port=8000, reload=True)
+    uvicorn.run("web_server:app", host="127.0.0.1", port=8000, reload=True)
